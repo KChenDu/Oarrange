60b3cc9e794c16172e893ee604859bfd6d8c9010
Orange <kelvenchendu@gmail.com>
2023-01-20 04:54:02 +0800

    stage backup

--- app/src/main/java/com/example/oarrange/GameActivity.java
+++ app/src/main/java/com/example/oarrange/GameActivity.java
@@ -1,37 +1,50 @@
 package com.example.oarrange;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
-    GridView layer0;
-    GridView layer1;
-
-    final Integer[][] layers = {
-            {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24},
-            {0,1,2,3,4,5,6,7,8,9,10}
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+/*
+        GridLayout gridLayout = new GridLayout(this);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
+        layoutParams.width = GridLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.height = GridLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.setMargins(0, 0, 0, 0);
+        gridLayout.setLayoutParams(layoutParams);
+        gridLayout.setColumnCount(5);
+        gridLayout.setColumnCount(5);
+ */
+        //ImageView imageView = new ImageView(this);
+        //imageView.setImageResource(R.drawable.donut_circle);
+        //imageView.setLayoutParams(new ViewGroup.LayoutParams(80, 80));
+/*
+        GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
+        GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
+        GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(
+                rowSpan, colSpan
+        );*/
 
-        layer0 = findViewById(R.id.layer0);
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, layers[0]);
-        layer0.setAdapter(adapter);
+        // GridLayout layer0 = (GridLayout) findViewById(R.id.layer0);
 
-        layer1 = findViewById(R.id.layer1);
-        adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, layers[1]);
-        layer1.setAdapter(adapter);
+        // GridLayoutManager gridLayoutManager = new GridLayoutManager(this, );
 
     }
 }
