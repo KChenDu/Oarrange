8ca7a89cf9037d603ca61709298094b794d625be
Orange <kelvenchendu@gmail.com>
2023-01-20 20:33:24 +0800

    implement multiple layer for cards

--- app/src/main/java/com/example/oarrange/GameActivity.java
+++ app/src/main/java/com/example/oarrange/GameActivity.java
@@ -34,26 +34,48 @@ public class GameActivity extends AppCompatActivity {
 
         // Card card1 = new Card(0, new Pair<Integer, Integer>(0, 0), 0);
 
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this));
+
         // Add first icon
         ImageView mImageView1 = new ImageView(this);
-        mImageView1.setLayoutParams(new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this)));
+        mImageView1.setLayoutParams(params);
+        mImageView1.setZ(0);
         mImageView1.setImageResource(R.drawable.donut_circle);
         mFrameLayout.addView(mImageView1);
 
         // Add second icon
         ImageView mImageView2 = new ImageView(this);
-        mImageView2.setLayoutParams(new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this)));
+        mImageView2.setLayoutParams(params);
         mImageView2.setX(dp2px(80, this));
+        mImageView2.setZ(0);
         mImageView2.setImageResource(R.drawable.icecream_circle);
         mFrameLayout.addView(mImageView2);
 
         // Add third icon above the previous two
         ImageView mImageView3 = new ImageView(this);
-        mImageView3.setLayoutParams(new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this)));
+        mImageView3.setLayoutParams(params);
         mImageView3.setX(dp2px(40, this));
         mImageView3.setY(dp2px(40, this));
+        mImageView3.setZ(1);
         mImageView3.setImageResource(R.drawable.froyo_circle);
         mFrameLayout.addView(mImageView3);
+
+        // Add fourth icon
+        ImageView mImageView4 = new ImageView(this);
+        mImageView4.setLayoutParams(params);
+        mImageView4.setY(dp2px(80, this));
+        mImageView4.setZ(0);
+        mImageView4.setImageResource(R.drawable.ic_launcher_background);
+        mFrameLayout.addView(mImageView4);
+
+        // Add fifth icon
+        ImageView mImageView5 = new ImageView(this);
+        mImageView5.setLayoutParams(params);
+        mImageView5.setX(dp2px(80, this));
+        mImageView5.setY(dp2px(80, this));
+        mImageView5.setZ(0);
+        mImageView5.setImageResource(R.drawable.icecream_circle);
+        mFrameLayout.addView(mImageView5);
     }
 
     static class Card extends AppCompatActivity {
