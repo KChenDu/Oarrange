43ff2793c32228c70c2c63b056d00ad220fd3a9a
Orange <kelvenchendu@gmail.com>
2023-01-20 18:17:21 +0800

    implement Card class

--- app/src/main/java/com/example/oarrange/GameActivity.java
+++ app/src/main/java/com/example/oarrange/GameActivity.java
@@ -3,18 +3,24 @@ package com.example.oarrange;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -22,29 +28,27 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-/*
-        GridLayout gridLayout = new GridLayout(this);
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
-        layoutParams.width = GridLayout.LayoutParams.MATCH_PARENT;
-        layoutParams.height = GridLayout.LayoutParams.MATCH_PARENT;
-        layoutParams.setMargins(0, 0, 0, 0);
-        gridLayout.setLayoutParams(layoutParams);
-        gridLayout.setColumnCount(5);
-        gridLayout.setColumnCount(5);
- */
-        //ImageView imageView = new ImageView(this);
-        //imageView.setImageResource(R.drawable.donut_circle);
-        //imageView.setLayoutParams(new ViewGroup.LayoutParams(80, 80));
-/*
-        GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
-        GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
-        GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(
-                rowSpan, colSpan
-        );*/
 
-        // GridLayout layer0 = (GridLayout) findViewById(R.id.layer0);
+        Card card1 = new Card(0, new Pair<Integer, Integer>(0, 0), 0);
 
-        // GridLayoutManager gridLayoutManager = new GridLayoutManager(this, );
+        ImageView mImageView = new ImageView(this);
 
+
+        FrameLayout mFrameLayout = findViewById(R.id.board);
+        mFrameLayout.addView(mImageView);
+    }
+
+    static class Card extends AppCompatActivity {
+
+        public Integer type;
+        public Pair<Integer, Integer> position;
+        public Integer layer;
+
+        public Card(Integer type, Pair<Integer, Integer> position, Integer layer)
+        {
+            this.type = type;
+            this.position = position;
+            this.layer = layer;
+        }
     }
 }
