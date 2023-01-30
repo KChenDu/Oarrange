e6dbd18262b0369576f743cff4fc411b1b9f97cb
Orange <kelvenchendu@gmail.com>
2023-01-30 17:29:36 +0800

    create Card class + implement put method

--- app/src/main/java/com/example/oarrange/GameActivity.java
+++ app/src/main/java/com/example/oarrange/GameActivity.java
@@ -24,72 +24,52 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class GameActivity extends AppCompatActivity {
+    private FrameLayout mFrameLayout;
+    private FrameLayout.LayoutParams mLayoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        FrameLayout mFrameLayout = findViewById(R.id.board);
+        mFrameLayout = findViewById(R.id.board);
+        mLayoutParams = new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this));
 
-        // Card card1 = new Card(0, new Pair<Integer, Integer>(0, 0), 0);
-
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(dp2px(80, this), dp2px(80, this));
-
-        // Add first icon
-        ImageView mImageView1 = new ImageView(this);
-        mImageView1.setLayoutParams(params);
-        mImageView1.setZ(0);
-        mImageView1.setImageResource(R.drawable.donut_circle);
-        mFrameLayout.addView(mImageView1);
-
-        // Add second icon
-        ImageView mImageView2 = new ImageView(this);
-        mImageView2.setLayoutParams(params);
-        mImageView2.setX(dp2px(80, this));
-        mImageView2.setZ(0);
-        mImageView2.setImageResource(R.drawable.icecream_circle);
-        mFrameLayout.addView(mImageView2);
-
-        // Add third icon above the previous two
-        ImageView mImageView3 = new ImageView(this);
-        mImageView3.setLayoutParams(params);
-        mImageView3.setX(dp2px(40, this));
-        mImageView3.setY(dp2px(40, this));
-        mImageView3.setZ(1);
-        mImageView3.setImageResource(R.drawable.froyo_circle);
-        mFrameLayout.addView(mImageView3);
-
-        // Add fourth icon
-        ImageView mImageView4 = new ImageView(this);
-        mImageView4.setLayoutParams(params);
-        mImageView4.setY(dp2px(80, this));
-        mImageView4.setZ(0);
-        mImageView4.setImageResource(R.drawable.ic_launcher_background);
-        mFrameLayout.addView(mImageView4);
-
-        // Add fifth icon
-        ImageView mImageView5 = new ImageView(this);
-        mImageView5.setLayoutParams(params);
-        mImageView5.setX(dp2px(80, this));
-        mImageView5.setY(dp2px(80, this));
-        mImageView5.setZ(0);
-        mImageView5.setImageResource(R.drawable.icecream_circle);
-        mFrameLayout.addView(mImageView5);
+        Card card1 = new Card(0, new Pair<Integer, Integer>(0, 0), 0);
+        put(card1, this);
+        Card card2 = new Card(0, new Pair<Integer, Integer>(0, 1), 0);
+        put(card2, this);
+        Card card3 = new Card(0, new Pair<Integer, Integer>(1, 0), 0);
+        put(card3, this);
+        Card card4 = new Card(0, new Pair<Integer, Integer>(1, 1), 0);
+        put(card4, this);
+        Card card5 = new Card(0, new Pair<Integer, Integer>(0, 0), 1);
+        put(card5, this);
     }
 
-    static class Card extends AppCompatActivity {
 
+
+    private static class Card extends AppCompatActivity {
         public Integer type;
         public Pair<Integer, Integer> position;
         public Integer layer;
 
-        public Card(Integer type, Pair<Integer, Integer> position, Integer layer)
-        {
+        public Card(Integer type, Pair<Integer, Integer> position, Integer layer) {
             this.type = type;
             this.position = position;
             this.layer = layer;
         }
+    }
+
+    private void put(Card card, Context context) {
+        ImageView mImageView = new ImageView(context);
+        mImageView.setLayoutParams(mLayoutParams);
+        Integer layer = card.layer;
+        mImageView.setX(dp2px(layer * 40 + card.position.first * 80, this));
+        mImageView.setY(dp2px(layer * 40 + card.position.second * 80, this));
+        mImageView.setZ(card.layer);
+        mImageView.setImageResource(R.drawable.donut_circle);
+        mFrameLayout.addView(mImageView);
     }
 
     public static int dp2px(int dp, Context context) {
